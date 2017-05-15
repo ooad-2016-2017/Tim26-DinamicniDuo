8068fda1d1e67f5c5ae633db0b9bc366981c0e84
Adina Omerović <Adina Omerovi>
2017-05-15 22:28:59 +0200

    Dodane 4 forne: Login, MainPage, Register i ForgotPass.

--- Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Login.xaml.cs
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Login.xaml.cs
@@ -13,25 +13,38 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ResidenceInnEnjoyYourStay
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
-            
             this.InitializeComponent();
+        }
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Register), null);
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Login), null);
+            this.Frame.Navigate(typeof(MainPage), null);
         }
 
+        private void HyperlinkButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ForgotPass), null);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
     }
 }
+

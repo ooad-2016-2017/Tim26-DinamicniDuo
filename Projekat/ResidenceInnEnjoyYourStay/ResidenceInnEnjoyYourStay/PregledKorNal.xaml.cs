21f1827bfdddd8d8837230411339834b4f4693db
hhodzic1 <video70230@gmail.com>
2017-05-17 00:53:15 +0200

    Dodane 3 nove forme RezervacijaSmjestaja,PregledKoNal,PregledObjavaAdmin

---
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/PregledKorNal.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResidenceInnEnjoyYourStay
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/RezervacijaSmjestaja.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResidenceInnEnjoyYourStay
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

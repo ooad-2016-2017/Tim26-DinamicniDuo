efe7bb14cb43c76307182a84a0bce442fe325337
Adina Omerović <Adina Omerovi>
2017-05-22 22:01:36 +0200

    Updated: Projekat/ResidenceInnEnjoyYourStay

---
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Models/Osoba.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OOAD1.ResidenceInnBaza.Models
+{
+    public abstract class Osoba : MainViewModelBase
+    {
+        private String ime;
+        private String prezime;
+        private String email;
+        private String user;
+        private String password;
+        private DateTime datumRodjenja;
+
+        public String Ime
+        {
+            get
+            {
+                return ime;
+            }
+            set
+            {
+                if (value != ime)
+                {
+                    ime = value;
+                    OnPropertyChanged("Ime");
+                }
+            }
+        }
+        public String Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+            set
+            {
+                if (value != prezime)
+                {
+                    prezime = value;
+                    OnPropertyChanged("Prezime");
+                }
+            }
+        }
+        public String Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (value != email)
+                {
+                    email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+        public String User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                if (value != user)
+                {
+                    user = value;
+                    OnPropertyChanged("User");
+                }
+            }
+        }
+        public String Pass
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (value != password)
+                {
+                    password = value;
+                    OnPropertyChanged("Pass");
+                }
+            }
+        }
+        public DateTime DatumRodjenja
+        {
+            get
+            {
+                return datumRodjenja;
+            }
+            set
+            {
+                if (value != datumRodjenja)
+                {
+                    datumRodjenja = value;
+                    OnPropertyChanged("DatumRodjenja");
+                }
+            }
+        }
+        public Osoba(string im, string prez, DateTime datR, string us, string pass, string em)
+        {
+            Ime = im;
+            Prezime = prez;
+            Email = em;
+            User = us;
+            Pass = pass;
+            DatumRodjenja = datR;
+        }
+        public Osoba()
+        {
+
+        }
+    }
+}
---
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Models/SponzorisaniOglasi.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAD1.ResidenceInnBaza.Models
+{
+    public class SponzorisaniOglasi : BazniOglasi
+    {
+        private int cijenaSponzorstva;
+        private int rokTrajanja;
+        private String nacinSponzorisanja;
+        private DateTime pocetakSponzorisanja;
+
+        public int CijenaSponzorstva
+        {
+            get
+            {
+                return cijenaSponzorstva;
+            }
+            set
+            {
+                if (value != cijenaSponzorstva)
+                {
+                    cijenaSponzorstva = value;
+                }
+            }
+        }
+        public int RokTrajanja
+        {
+            get
+            {
+                return rokTrajanja;
+            }
+            set
+            {
+                if (value != rokTrajanja)
+                {
+                    rokTrajanja = value;
+                }
+            }
+        }
+        public String NacinSponzorisanja
+        {
+            get
+            {
+                return nacinSponzorisanja;
+            }
+            set
+            {
+                if (value != nacinSponzorisanja)
+                {
+                    nacinSponzorisanja = value;
+                }
+            }
+        }
+        public DateTime PocetakSponzorisanja
+        {
+            get
+            {
+                return pocetakSponzorisanja;
+            }
+            set
+            {
+                if (value != pocetakSponzorisanja)
+                {
+                    pocetakSponzorisanja = value;
+                }
+            }
+        }
+
+    }
+}

7af99962e30e67e01c19112f1fe691cff8c030dc
Adina Omerović <Adina Omerovi>
2017-05-31 03:06:43 +0200

    Updated : Projekat/ResidenceInnEnjoyYourStay - Azure konekcija

--- Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Views/Registracija.xaml.cs
+++ Projekat/ResidenceInnEnjoyYourStay/ResidenceInnEnjoyYourStay/Views/Registracija.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ResidenceInnEnjoyYourStay.ViewModels;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using ResidenceInnEnjoyYourStay.Azure;
+using ResidenceInnEnjoyYourStay.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -40,6 +42,33 @@ namespace ResidenceInnEnjoyYourStay.Views
                 //Do your stuff
             }
         }
+        IMobileServiceTable<RegistrovaniKorisnik> userTableObj = App.MobileService.GetTable<RegistrovaniKorisnik>();
+        private void buttonRegistrujSe_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RegistrovaniKorisnik obj = new RegistrovaniKorisnik();
+                obj.ime = textBoxIme.Text;
+                obj.prezime = textBoxPrezime.Text;
+                obj.email = textBoxEmail.Text;
+                obj.password = passwordBoxSifra.Password;
+                obj.username = textBoxKorisnik.Text;
+                obj.datumRodjenja = new DateTime(1995, 5, 15);
+                userTableObj.InsertAsync(obj);
+                MessageDialog msgDialog = new MessageDialog("Uspješno ste registrovani.");
 
+                msgDialog.ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageDialog msgDialogError = new MessageDialog("Error : " +
+               ex.ToString());
+                msgDialogError.ShowAsync();
+            }
+        }
+
+    
     }
 }
+
+//Command="{Binding RegisterCommand}"
